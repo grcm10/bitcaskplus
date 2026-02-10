de24412f3672767fdda99bd4ce7909de524dbc29
grcm10 <grcm10@outlook.com>
2026-02-10 17:21:04 +0900

    Improve writer functions

--- src/db_write/writer.rs
+++ src/db_write/writer.rs
@@ -2,31 +2,36 @@ use crate::{BitCaskPlus, COMPACTION_THRESHOLD, Command, CommandPos, Result};
 use std::io::{self, Seek, SeekFrom, Write};
 
 impl BitCaskPlus {
+    fn write_data(&mut self, cmd: &Command) -> io::Result<CommandPos> {
+        match self.writer.flush() {
+            Ok(_) => {
+                let pos = self.writer.seek(SeekFrom::End(0))?;
+                let json_data =
+                    serde_json::to_string(&cmd).map_err(|e| io::Error::other(e.to_string()))?;
+                let json_data_len = json_data.len() as u64;
+                let checksum = crc32fast::hash(json_data.as_bytes());
+                // CRC(4) + Len(8) + Data(N)
+                self.writer.write_all(&checksum.to_le_bytes())?;
+                self.writer.write_all(&json_data_len.to_le_bytes())?; // Little-Endian
+                self.writer.write_all(json_data.as_bytes())?;
+                self.writer.flush()?;
+                Ok(CommandPos {
+                    pos,
+                    len: 4 + 8 + json_data_len,
+                })
+            }
+            Err(e) => Err(e),
+        }
+    }
+
     pub fn set(&mut self, key: String, val: String) -> Result<()> {
         let cmd = Command::Set {
             key: key.clone(),
             value: val,
         };
 
-        self.writer.flush()?;
-        let pos = self.writer.seek(SeekFrom::End(0))?;
-        let json_data = serde_json::to_string(&cmd).map_err(|e| io::Error::other(e.to_string()))?;
-        let json_data_len = json_data.len() as u64;
-        let checksum = crc32fast::hash(json_data.as_bytes());
-        // CRC(4) + Len(8) + Data(N)
-        self.writer.write_all(&checksum.to_le_bytes())?;
-        self.writer.write_all(&json_data_len.to_le_bytes())?; // Little-Endian
-        self.writer.write_all(json_data.as_bytes())?;
-        self.writer.flush()?;
-
-        let record_len = 4 + 8 + json_data_len;
-        if let Some(old_pos) = self.map.insert(
-            key,
-            CommandPos {
-                pos,
-                len: record_len,
-            },
-        ) {
+        let cmd_pos = self.write_data(&cmd)?;
+        if let Some(old_pos) = self.map.insert(key, cmd_pos) {
             self.uncompacted += old_pos.len;
         }
 
@@ -46,18 +51,9 @@ impl BitCaskPlus {
             key: key_str.clone(),
         };
 
-        self.writer.flush()?;
-        let json_data = serde_json::to_string(&cmd).map_err(|e| io::Error::other(e.to_string()))?;
-        let json_data_len = json_data.len() as u64;
-        let checksum = crc32fast::hash(json_data.as_bytes());
-        // CRC(4) + Len(8) + Data(N)
-        self.writer.write_all(&checksum.to_le_bytes())?;
-        self.writer.write_all(&json_data_len.to_le_bytes())?; // Little-Endian
-        self.writer.write_all(json_data.as_bytes())?;
-        self.writer.flush()?;
-
+        let cmd_pos = self.write_data(&cmd)?;
         if let Some(old_pos) = self.map.remove(&key_str) {
-            self.uncompacted += old_pos.len + (4 + 8 + json_data_len);
+            self.uncompacted += old_pos.len + cmd_pos.len;
         }
 
         if self.uncompacted > COMPACTION_THRESHOLD {
